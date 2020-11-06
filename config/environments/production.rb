b2a59a29eb53ecb68fe3982484aac6756f8e53fd
Luciano Cesar <lucianocordeiro1991@hotmail.com>
2020-11-05 21:28:41 -0300

    configs production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.serve_static_assets = true
+
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
